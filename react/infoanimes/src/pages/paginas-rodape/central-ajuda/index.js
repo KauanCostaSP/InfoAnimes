f7f61d8cc8a1a430c9b09fe5b094358f18df060f
Isasouza00 <isasouza20190701@gmail.com@gmail.com>
2021-10-05 16:11:50 -0300

    Nega veia prenha

---
+++ react/infoanimes/src/pages/paginas-rodape/central-ajuda/index.js
@@ -0,0 +1,122 @@
+import { CentralAjudaComp } from "./styled";
+
+export default function Index() {
+    return(
+        <CentralAjudaComp> 
+            <CabecalhoComp/>
+
+            <div class="conteudo_central">
+            <div class="seta"><img src="../../../assets/images/Arrow 1.svg" alt=""/></div>
+
+            <div class="caixa_perguntas">
+                <div class="caixa_pergunta_resposta">
+                    <div class="duvida">
+                        Como entrar em um servidor?
+                        <svg
+                              viewBox="0 0 18 18"
+                              role="img"
+                              aria-hidden="false"
+                              aria-label="Fotografia do anúncio em destaque anterior"
+                              focusable="false"
+                              style="
+                                height: 16px;
+                                width: 16px;
+                                display: block;
+                                fill: #ffffff;
+                              "
+                            >
+                              <path
+                                d="m13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z"
+                                fill-rule="evenodd"
+                              ></path>
+                        </svg>
+                    </div>
+
+                    <div class="resposta">. O acesso ao servidor é dado ao clicar no botão...</div>
+
+                    <div class="curtidas">
+                        <div class="like"><img src="../../../assets/images/like.png" alt=""/>17</div>
+                        <div class="deslike"><img src="../../../assets/images/deslike.png" alt=""/><div class="cont_deslike">0</div></div>
+                    </div>
+                    <hr/>
+                </div>
+
+
+                <div class="caixa_pergunta">
+                    <div class="duvida">
+                        Como entrar em um servidor?
+                        <svg
+                              viewBox="0 0 18 18"
+                              role="img"
+                              aria-hidden="false"
+                              aria-label="Fotografia do anúncio em destaque anterior"
+                              focusable="false"
+                              style="
+                                height: 16px;
+                                width: 16px;
+                                display: block;
+                                fill: #ffffff;
+                              "
+                            >
+                              <path
+                                d="m13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z"
+                                fill-rule="evenodd"
+                              ></path>
+                        </svg>
+                    </div>
+                    <hr/>
+                </div>
+
+                <div class="caixa_pergunta">
+                    <div class="duvida">
+                        Como entrar em um servidor?
+                        <svg
+                              viewBox="0 0 18 18"
+                              role="img"
+                              aria-hidden="false"
+                              aria-label="Fotografia do anúncio em destaque anterior"
+                              focusable="false"
+                              style="
+                                height: 16px;
+                                width: 16px;
+                                display: block;
+                                fill: #ffffff;
+                              "
+                            >
+                              <path
+                                d="m13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z"
+                                fill-rule="evenodd"
+                              ></path>
+                        </svg>
+                    </div>
+                    <hr/>
+                </div>
+
+                <div class="caixa_pergunta">
+                    <div class="duvida">
+                        Como entrar em um servidor?
+                        <svg
+                              viewBox="0 0 18 18"
+                              role="img"
+                              aria-hidden="false"
+                              aria-label="Fotografia do anúncio em destaque anterior"
+                              focusable="false"
+                              style="
+                                height: 16px;
+                                width: 16px;
+                                display: block;
+                                fill: #ffffff;
+                              "
+                            >
+                              <path
+                                d="m13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z"
+                                fill-rule="evenodd"
+                              ></path>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </CentralAjudaComp>
+    )
+}
