1383d67b9ceb7b4b06db15c0e858b443b146f845
Davi <davii.clink@gmail.com>
2021-11-06 14:42:02 -0300

    api INICIADA PORRA

--- api/src/index.js
+++ api/src/index.js
@@ -8,7 +8,25 @@ app.use(express.json());
 
 
 
+app.post('/login', async (req, resp) => {
+    try {
+        
+        let { email, senha } = req.body;
+
+        let r = await db.infod_tif_usuario.create({
+            ds_email: email,
+            ds_senha: senha
+        })
+
+        resp.send(r)
+
+    } catch (e) {
+        resp.send({ error: e.toString() })
+    }
+})
+
+
 
 
 app.listen(process.env.PORT,
-x => console.log(`Server up at port ${process.env.PORT}`))
+    x => console.log(`>> Server up at port ${process.env.PORT}`))
