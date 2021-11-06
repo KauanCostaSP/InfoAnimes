8be0bd9629fe1b3279e9ed914d6480375b7f35db
Isasouza00 <isasouza20190701@gmail.com@gmail.com>
2021-11-06 04:47:13 -0300

    Tio Clink69

---
+++ react/infoanimes/src/pages/perfil-usuario/configurar-perfil/index.js
@@ -0,0 +1,15 @@
+export default function config_perfil() {
+    <Container>
+        <Cabecalho/>
+
+
+        <div class="conteudo_edit">
+            <div class="usu_alt"></div>
+            <div class="campos"></div>
+            <div class="botoes"></div>
+        </div>
+
+
+        <Rodape/>
+    </Container>
+}
