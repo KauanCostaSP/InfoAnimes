c9abd016ea189aee9ce75714f7f458242b1e96f6
Isasouza00 <isasouza20190701@gmail.com@gmail.com>
2021-11-06 15:56:57 -0300

    Força guerreiro

--- api/src/index.js
+++ api/src/index.js
@@ -28,5 +28,35 @@ app.post('/login', async (req, resp) => {
 
 
 
+app.post('/catalogo', async (req, resp) => {
+    try {
+        let { anime, classificacao, temporadas, genero, estrelando, sinopse, sobre,enredo, capa, postagem, ano, video1, video2 } = req.body;
+
+    let a = await db.infod_tif_animes.create({
+            nm_anime: anime,
+            ds_classificação: classificacao,
+            ds_temporadas: temporadas,
+            ds_genero: genero,
+            ds_estrelando: estrelando,
+            ds_sinopse: sinopse,
+            ds_sobre: sobre,
+            ds_enredo: enredo,
+            ds_capa: capa,
+            dt_postagem: postagem,
+            dt_ano: ano,
+            ds_video1: video1,
+            ds_video2: video2
+    })
+
+    resp.send(a);
+
+}   catch(b) {
+        resp.send({erro: b.toString() })
+    }
+})
+
+
+
+
 app.listen(process.env.PORT,
     x => console.log(`>> Subiu esse caralho na porta ${process.env.PORT}`))
