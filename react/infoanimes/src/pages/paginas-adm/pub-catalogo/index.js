25a6ee0eb634471f525e238f5d1bc22f26a8e7c0
Isasouza00 <isasouza20190701@gmail.com@gmail.com>
2021-11-08 17:59:38 -0300

    foda a dor do Kaique coitado né

--- react/infoanimes/src/pages/paginas-adm/pub-catalogo/index.js
+++ react/infoanimes/src/pages/paginas-adm/pub-catalogo/index.js
@@ -1,9 +1,9 @@
-import { Container } from "../../Inicial/styled"
+import { Container } from "./styled"
 
 export default function publi_catalogo() {
     return (
         <Container>
-            <div className="alterar_fundo"><button> Alterar plano de fundo </button></div>
+            <div className="alterar_fundo"><button> Adicionar plano de fundo </button></div>
             <div className="ano"><input type="number" id="ano" name="ano" placeholder="ano de lançamento" max="2021"/></div>
             <div className="classificacao">| Classificação<input type="number" id="classif" name="classificacao" placeholder="Classificação indicativa"/></div>
             <div className="temporadas">|<input type="number" id="temp" name="temporadas" placeholder="Número de temporadas por anime" min="1"/>Temporadas</div>
---
+++ react/infoanimes/src/pages/paginas-adm/pub-catalogo/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.div `
+    div {
+        color: #ffffff;
+        background-color: #0d0d0d;
+    }
+
+`
+
+
+export {Container}
