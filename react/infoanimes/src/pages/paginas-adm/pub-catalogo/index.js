2824f74b752f7344ff4e26772db1f854b35b8ce1
Isasouza00 <isasouza20190701@gmail.com@gmail.com>
2021-11-08 18:13:13 -0300

    pedido de socorro, ass: Isa

--- react/infoanimes/src/pages/paginas-adm/pub-catalogo/index.js
+++ react/infoanimes/src/pages/paginas-adm/pub-catalogo/index.js
@@ -7,13 +7,13 @@ export default function publi_catalogo() {
             <div className="ano"><input type="number" id="ano" name="ano" placeholder="ano de lançamento" max="2021"/></div>
             <div className="classificacao">| Classificação<input type="number" id="classif" name="classificacao" placeholder="Classificação indicativa"/></div>
             <div className="temporadas">|<input type="number" id="temp" name="temporadas" placeholder="Número de temporadas por anime" min="1"/>Temporadas</div>
-            <div className="produtora">|<input type="text" id="produtora" size="40" placeholder="Nome"/></div>
-            <div className="generos">Gênero<input type="text" id="generos" size="40" placeholder="Nome"/></div>
-            <div className="estrelando">Estrelando<input type="text" id="estrelando" size="40" placeholder="Nome"/></div>
-            <div className="sinopse">Sinopse<input type="text" id="sinopse" size="40" placeholder="Nome"/></div>
-            <div className="sobre">Sobre<input type="text" id="sobre" size="40" placeholder="Nome"/></div>
-            <div className="enredo">Enredo<input type="text" id="enredo" size="40" placeholder="Nome"/></div>
-            <div className="nome_anime_trailer">Videos | <input type="nome_anime" id="produtora" size="40" placeholder="Nome"/></div>
+            <div className="produtora">|<input type="text" id="produtora" size="40" placeholder="Nome da produtora"/></div>
+            <div className="generos">Gênero<input type="text" id="generos" size="40" placeholder="Gêneros"/></div>
+            <div className="estrelando">Estrelando<input type="text" id="estrelando" size="40" placeholder="Estrelando"/></div>
+            <div className="sinopse">Sinopse<input type="text" id="sinopse" size="40" placeholder="Sinopse"/></div>
+            <div className="sobre">Sobre<input type="text" id="sobre" size="40" placeholder="Sobre"/></div>
+            <div className="enredo">Enredo<input type="text" id="enredo" size="40" placeholder="Enredo"/></div>
+            <div className="nome_anime_trailer">Videos | <input type="nome_anime" id="produtora" size="40" placeholder="Nome do Anime"/></div>
             <div className="video1"><button> Adicionar Vídeo </button></div>
             <div className="vieo2"><button> Adicionar Vídeo </button></div>
             <div className="t1-titulo"><button> Cadastrar </button></div>
