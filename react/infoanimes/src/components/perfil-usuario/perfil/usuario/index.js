d0a02c6d585820ee10d3e4c9acb39f1707fd92d1
Isasouza00 <isasouza20190701@gmail.com@gmail.com>
2021-11-15 02:02:07 -0300

    Jesus amado

--- react/infoanimes/src/components/perfil-usuario/perfil/usuario/index.js
+++ react/infoanimes/src/components/perfil-usuario/perfil/usuario/index.js
@@ -8,11 +8,11 @@ export default function Index () {
 
         <div className="box-1"> 
             <div className="cabecalho_box-1">
-                <Link className="menu_box-1"> <img src="../../../assets/images/menu.jpg" alt="" /></Link>
+                <Link className="menu_box-1"> <img src="../../../assets/images/menu.jpg" alt=""/> </Link>
             </div>
 
             <div className="usuario_box-1">
-                <div className="perfil_box-1"><img src="../../../assets/images/perf_ket.png" alt="Foto de Perfil"/></div>
+                <div className="perfil_box-1"> <img src="../../../assets/images/perf_ket.png" alt="Foto de Perfil"/> </div>
                 <div className="nick_box-1">Ketellyn_Otako_</div>
             </div>
 
@@ -26,26 +26,25 @@ export default function Index () {
                     <div className="retangulo-branco"></div>
                     <div className="text-retangulo-branco">Biografia</div> 
                     <div className="info_biografia">
-                        <div className="descricao">otako | army | shonen | kawaii
-                            Estou nessa app para amigar!</div>
+                        <div className="descricao">otako | army | shonen | kawaii Estou nessa app para amigar!</div>
                     </div>
                 </div>
             </div>
 
             <div className="comunidades-e-chats">
                 <div className="titulo-box-2"> 
-                    <div className="retangulo-branco"></div>
+                    <div className="retangulo-branco"> </div>
                     <div className="text-retangulo-branco">Minhas comunidades | Chats online</div>
                     
                 </div>
                 <div className="chats">
-                    <div className="img_chats"><img src="../../../assets/images/perf_2.png" alt="Perfil"/></div>
-                    <div className="img_chats"><img src="../../../assets/images/perf_3.png" alt="Perfil"/></div>     
-                    <div className="img_chats"><img src="../../../assets/images/perf_4.png" alt="Perfil"/></div> 
-                    <div className="img_chats"><img src="../../../assets/images/perf_5.png" alt="Perfil"/></div>  
-                    <div className="img_chats"><img src="../../../assets/images/perf_6.png" alt="Perfil"/></div>
-                    <div className="img_chats"><img src="../../../assets/images/perf_7.png" alt="Perfil"/></div>   
-                    <div className="img_chats"><img src="../../../assets/images/perf_8.png" alt="Perfil"/></div> 
+                    <div className="img_chats"> <img src="../../../assets/images/perf_2.png" alt="Perfil"/> </div>
+                    <div className="img_chats"> <img src="../../../assets/images/perf_3.png" alt="Perfil"/> </div>     
+                    <div className="img_chats"> <img src="../../../assets/images/perf_4.png" alt="Perfil"/> </div> 
+                    <div className="img_chats"> <img src="../../../assets/images/perf_5.png" alt="Perfil"/> </div>  
+                    <div className="img_chats"> <img src="../../../assets/images/perf_6.png" alt="Perfil"/> </div>
+                    <div className="img_chats"> <img src="../../../assets/images/perf_7.png" alt="Perfil"/> </div>   
+                    <div className="img_chats"> <img src="../../../assets/images/perf_8.png" alt="Perfil"/> </div> 
                 </div>
             </div>
         </div>
