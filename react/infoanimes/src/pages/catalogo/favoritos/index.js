5ea622e2ee010dbfad9b932a52a11e07c823103a
Isasouza00 <isasouza20190701@gmail.com@gmail.com>
2021-11-15 21:00:24 -0300

    Foco, força e tomagem no cu

--- react/infoanimes/src/pages/catalogo/favoritos/index.js
+++ react/infoanimes/src/pages/catalogo/favoritos/index.js
@@ -10,7 +10,41 @@ import Api from "../../service/api";
 const api = new Api();
 
 
+function lerUsuarioLogado(navigation) {
+    let logado = Cookies.get('usuario-logado')
+    if (!logado) {
+        return false
+    }
+
+    let usuarioLogado = JSON.parse(logado);
+    return usuarioLogado;
+}
+
+
 export default function Favoritos() {
+
+    const navigation = useHistory();
+
+    let usuarioLogado = lerUsuarioLogado(navigation) || {};
+
+    const [usu] = useState(usuarioLogado);
+
+    const [favoritos, setFavoritos] = useState([]);
+
+    async function mostrarFavoritos() {
+        if (usu.id_usuario) {
+            let v = await api.mostrarFavoritos(usu.id_usuario);
+            console.log(v)
+            setFavoritos(v);
+        }
+    }
+
+    useEffect(() => {
+        mostrarFavoritos();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+
     return (
         <Container>
             <Cabecalho />
@@ -19,15 +53,21 @@ export default function Favoritos() {
             <div className="back">               
             <div className="user"> 
                 <span className="perfil"><a href="../../PERFIL-USUARIO/perfil/index.html"><img src="../../../assets/images/perfil_ket_otaku.png" alt="perfil"/></a></span>
-                <div className="nick"></div>
+                <div className="nick"> value={usu.nm_usuario} </div>
                 <div className="pontos">...</div>
             </div>
 
             <div className="animes">
                         <Anime
-                            img="/assets/images/Evangelion.png"
-                            nome="Comp"
+                            img={}
+                            nome={}
                         />
+
+                    {favoritos.map(anime =>
+                        <BoxProduto
+                            key={item.id}
+                            info={item} />
+                    )}
                 <div className="anime"><img src="../../../assets/images/Evangelion.png" alt="Evangelion"/><div className="titulo"><a href="../../CATALOGO/pagina-catalogo/index_evangelion.html" >Neon Genesis...</a></div></div>
             </div>
             </div>
